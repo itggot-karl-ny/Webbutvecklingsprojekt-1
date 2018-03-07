f2cad98ef2cc44e839a16bc987d2fb86adcd78b3
itggot-karl-ny <35599808+itggot-karl-ny@users.noreply.github.com>
2018-03-07 09:46:27 +0100

    raderat icke funktionell kod i javascript

--- webbprojekt 1/site/js/menu.js
+++ webbprojekt 1/site/js/menu.js
@@ -1,10 +1,4 @@
 function toggleMenu(){
     var element = document.querySelector(".menu");
     element.classList.toggle("show");
-
-    element = document.querySelector(".menudiv");
-    element.classList.toggle("expandDiv")
-
-    element = document.querySelector("main");
-    element.classList.toggle("fade")
 }
